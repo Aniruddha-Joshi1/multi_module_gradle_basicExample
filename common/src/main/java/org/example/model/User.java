419765e3c8b4a93e67ec464f925a267651ce76fc
aniruddha.joshi@kanerika.com-at-972218571398 <aniruddha.joshi.2003@gmail.com>
2025-05-06 13:01:38 +0530

    changed build files, created a post api

--- common/src/main/java/org/example/model/User.java
+++ common/src/main/java/org/example/model/User.java
@@ -1,5 +1,53 @@
 package org.example.model;
 
-public class User {
+import jakarta.persistence.*;
 
+import java.util.UUID;
+
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(name = "id")
+    private UUID id;
+
+    @Column(name = "username")
+    private String username;
+
+    @Column(name = "email")
+    private String email;
+
+    public User() {
+    }
+
+    public User(UUID id, String username, String email) {
+        this.id = id;
+        this.username = username;
+        this.email = email;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
 }
---
+++ userApp/src/main/java/org/example/controller/UserController.java
@@ -0,0 +1,24 @@
+package org.example.controller;
+
+import org.example.model.User;
+import org.example.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+    private final UserService userService;
+
+    @Autowired
+    public UserController(UserService userService){
+        this.userService = userService;
+    }
+
+    @PostMapping("/createUser")
+    public User createUser(@RequestBody User user){
+        userService.createUser(user);
+        return user;
+    }
+}
